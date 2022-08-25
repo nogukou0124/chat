e95964fd663c45480498f6c20ddac8dd282672fa
keitennis <kei.takahashi.meiji@gmail.com>
2022-08-25 18:58:36 +0900

    バックエンド更新

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -3,15 +3,19 @@
 // 投稿メッセージをサーバに送信する
 function publish() {
     // ユーザ名を取得
-    const userName = '';
+    const userName = $("#userName").val();
     // 入力されたメッセージを取得
-    const message = '';
-    // 投稿内容を送信
+    const message = $("#message").val();
 
+    // 投稿内容を送信
+    socket.emit("sendMessageEvent", [userName, message]);
+
+    // 投稿後に入力エリアを空にする。
+    $("#message").val("");
     return false;
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('receiveMessageEvent', function (data) {
+    $('#thread').prepend('<p>' + data[0] + "さん：" + data[1] + '</p>');
 });
