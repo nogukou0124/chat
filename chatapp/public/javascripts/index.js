5a74ba17951f7bc7af78c83204ee909f2e779a3a
miya498 <ugimaru262@gmail.com>
2022-08-25 22:47:12 +0900

    modify

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -3,12 +3,10 @@
 // チャットルームに入室する
 function enter() {
     // 入力されたユーザ名を取得する
-    const userName = $("#userName").val();
+    const userName = $('form').submit();
     // ユーザ名が未入力でないかチェックする
-    if (userName === "") {
-        alert("ユーザー名を入力してください。");
-    } else {
-        // 入室処理
-        $('form').submit();
-    }
+    if (userName === '') {
+        alert(userName);
+        }
+    
 }
