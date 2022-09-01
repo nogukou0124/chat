d83d26ff595a40bd36d1108b4085b98ebd395b0c
sakho13 <wecl.khotaro@gmail.com>
2022-09-01 18:45:24 +0900

    room作成用パス&リダイレクトロジック追加

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -10,5 +10,30 @@ function enter() {
     } else {
         // 入室処理
         $('form').submit();
-    }    
+    }
+}
+
+function test() {
+    console.log("test aaaaa")
+}
+
+function createRoom() {
+    const userName = $("#userName").val()
+
+    if (typeof userName !== "string" || userName.trim() === "") {
+        alert("ユーザー名を入力してください。");
+    } else {
+        const eCreateRoom = $(".createRoom")[0]
+        const eForm = document.createElement("form")
+        eForm.method = "POST"
+        eForm.action = "/create_room"
+        eForm.hidden = true
+        eCreateRoom.appendChild(eForm)
+        const eUserName = document.createElement("input")
+        eUserName.name = "userName"
+        eUserName.value = userName
+        eUserName.hidden = true
+        eForm.appendChild(eUserName)
+        eForm.submit()
+    }
 }
--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -5,19 +5,30 @@ const router = express.Router();
 
 // ログイン画面の表示
 router.get('/', function (_request, response, _next) {
-    response.render('index');
+    return response.render('index');
 });
 
 // チャット画面の表示
 router.post('/room', function (request, response, _next) {
-    console.log('room')
-    console.log('ユーザ名：' + request.body.userName);
-    response.render('room', { userName: request.body.userName });
+    return response.redirect('/')
 });
+
+router.post('/room/:roomId', function (request, response, _next) {
+    console.log('ユーザ名：' + request.body.userName);
+    console.log(request.params)
+    return response.render('room', { userName: request.body.userName });
+});
+
+// 部屋の新規作成用パス
+router.post('/create_room', function (req, res, _next) {
+    const now = new Date()
+    const nowStr = `${now.getFullYear()}${now.getMonth()}${now.getDate()}${now.getDay()}${now.getHours()}${now.getMinutes()}${now.getSeconds()}${now.getMilliseconds()}`
+    return res.redirect(307, `/room/${nowStr}`)
+})
 
 // URL直打ち回避用
 router.get('*', (_request, response, _next) => {
-    response.redirect('/')
+    return response.redirect('/')
 });
 
 module.exports = router;
