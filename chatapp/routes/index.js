eb11b622124e41ee0525ab5124b47501a231296e
sakho13 <wecl.khotaro@gmail.com>
2022-09-01 18:55:30 +0900

    軽微な修正

--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -9,18 +9,22 @@ router.get('/', function (_request, response, _next) {
 });
 
 // チャット画面の表示
-router.post('/room', function (request, response, _next) {
+router.post('/room/', function (_request, response, _next) {
     return response.redirect('/')
 });
 
 router.post('/room/:roomId', function (request, response, _next) {
     console.log('ユーザ名：' + request.body.userName);
-    console.log(request.params)
-    return response.render('room', { userName: request.body.userName });
+    console.log('ルームID: ' + request.params.roomId);
+
+    return response.render('room', {
+        userName: request.body.userName,
+        roomId: request.params.roomId,
+    });
 });
 
 // 部屋の新規作成用パス
-router.post('/create_room', function (req, res, _next) {
+router.post('/create_room', function (_req, res, _next) {
     const now = new Date()
     const nowStr = `${now.getFullYear()}${now.getMonth()}${now.getDate()}${now.getDay()}${now.getHours()}${now.getMinutes()}${now.getSeconds()}${now.getMilliseconds()}`
     return res.redirect(307, `/room/${nowStr}`)
