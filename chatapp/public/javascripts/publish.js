7bb061fc22950a39cb4972e8b66d37b8d3272d84
nogukou0124 <nogukou0124@docomo.ne.jp>
2022-09-01 18:28:03 +0900

    手紙機能追加

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -17,6 +17,22 @@ function publish() {
     return false;
 }
 
+function publishLetter() {
+    // ユーザ名を取得
+    const userName = $("#userName").val();
+    // 入力されたメッセージを取得
+    const message = $("#message").val();
+
+    if (typeof message === "string" && message.trim().length > 0) {
+        // 投稿内容を送信
+        setTimeout(() => {socket.emit("sendMessageEvent", [userName, message]);}, 5*1000);
+    }
+
+    // 投稿後に入力エリアを空にする。
+    $("#message").val("");
+    return false;
+}
+
 // サーバから受信した投稿メッセージを画面上に表示する
 socket.on('receiveMessageEvent', function (data) {
     $('#thread').prepend('<p>' + data[0] + "さん：" + data[1] + '</p>');
